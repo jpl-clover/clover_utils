5564643bfdb270114d92d5623e7867c100b195de
isaac-ward <isaacronaldward@gmail.com>
2022-04-05 11:53:11 -0700

    hubconf.py moved to root directory

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import numpy as np
 import torch
 
-import clover_utils.hubconf
+import hubconf
 
 # Check what entrypoints are available
-entrypoints = torch.hub.list('jpl-clover/clover_utils', force_reload=True)
+entrypoints = torch.hub.list('jpl-clover/clover_utils:main', force_reload=True)
 
 
 # Try loading a model and confirming it works by pushing through a tensor
 # and conducting a forward pass
-model = clover_utils.hubconf.finetuned_supervised_resnet()
+model = hubconf.finetuned_supervised_resnet()
 # Confirm that this outputs logits over 19 classes
 print(model(torch.zeros(1,3,227,227)).shape)
